cf8039473c95b2eb82e963d8d527c4ad5de9a8ce
Jena Brown <browjena@oregonstate.edu>
2023-10-17 18:28:48 -0400

    added login function

---
+++ client/src/ui/Login.jsx
@@ -0,0 +1,10 @@
+import '@passageidentity/passage-elements/passage-auth';
+
+function Login() {
+  return (
+    <div>
+     <passage-auth app-id="yKCRLJ0k5VFjLECim3ynXoSW"></passage-auth>
+     </div>
+  );
+}
+export default Login;
