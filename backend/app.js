be68bf693f774acad395571427534fa386f8126a
Jessica Hsiang <hsiang.jessica@gmail.com>
2023-10-17 14:17:27 -0700

    Setup app.use middleware and error handling. Updated README with instructions to start server

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const cors = require("cors");
+
+const { NotFoundError } = require("./expressErrors");
+
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 
 
+/** Handle 404 errors */
+app.use(function (req, res, next) {
+  return next(new NotFoundError());
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+  const status = err.status || 500;
+  const message = err.message;
+
+  return res.status(status).json({
+    error: {message, status}
+  });
+});
 
 
-
-module.exports = app;
+module.exports = app; 
