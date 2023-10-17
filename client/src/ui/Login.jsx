adb14caa627334d7130e5a7278aa732962e9f648
brownjena <143430132+brownjena@users.noreply.github.com>
2023-10-17 19:15:32 -0400

    Update Login.jsx
    took out app id

--- client/src/ui/Login.jsx
+++ client/src/ui/Login.jsx
@@ -3,7 +3,7 @@ import '@passageidentity/passage-elements/passage-login';
 function Login() {
   return (
       <div>
-        <passage-login app-id="yKCRLJ0k5VFjLECim3ynXoSW"></passage-login>
+        <passage-login app-id=""></passage-login>
       </div>
   );
 }
