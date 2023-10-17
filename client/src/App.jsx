5fd414fbcc418a6fb76817f44460e6c3c8408bbe
demitryedwards <dxyz773@gmail.com>
2023-10-16 22:11:44 -0400

    Add router and RouterProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
-import "./App.css";
+// import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([]);
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
