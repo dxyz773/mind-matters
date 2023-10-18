a68c3de088696fcf43c2eb9aa52b9e6ff4cc4841
demitryedwards <dxyz773@gmail.com>
2023-10-18 13:57:00 -0400

    Add wellnessResourceData file

---
+++ client/src/data/wellnessResourceData.js
@@ -0,0 +1,66 @@
+export const wellnessDate = [
+  {
+    topic: "meditation",
+    resources: [
+      {
+        statement: "What is meditation?",
+        resource: [
+          {
+            data: "The origins of meditation have roots that span thousands of years in Eastern traditions. Today, meditation is widely adopted throughout both Eastern and Western society.",
+          },
+          {
+            data: 'According to The National Center for Complementary and Integrative Health (NCCIH) of the National Institute of Health, "The term ‘meditation’ refers to a variety of practices that focus on mind and body integration” used to “calm the mind and enhance overall well-being."',
+          },
+        ],
+      },
+      {
+        statement: "Are there different types of meditation?",
+        resource: [
+          {
+            data: "Yes! The NCCIH explains that “some types of meditation involve maintaining mental focus on a particular sensation, such as breathing, a sound, a visual image, or a mantra, which is a repeated word or phrase.”",
+          },
+          {
+            data: "Other types of meditation can also include “the practice of mindfulness, which involves maintaining attention or awareness on the present moment without making judgments.” - Meditation And Mindfulness: What You Need To Know",
+          },
+        ],
+      },
+      {
+        statement: "Why do people meditate?",
+        resource: [
+          {
+            data: "Some practice meditation to manage stress, anxiety, depression, and pain, while others meditate to help improve blood pressure, insomnia, sleep quality, and for other health benefits.",
+          },
+        ],
+      },
+      {
+        statement: "What is the impact of meditation on mental wellbeing?",
+        resource: [
+          {
+            data: "There have been thousands of studies conducted around the world analyzing the impact of meditation on mental wellbeing, and many studies have found positive correlations between meditative practices and improved mental wellbeing.",
+          },
+          {
+            data: "One such study is the 2018 NCCIH-supported study where “142 groups of participants with diagnosed psychiatric disorders such as anxiety or depression examined mindfulness meditation approaches compared with no treatment and with established evidence-based treatments such as cognitive behavioral therapy and antidepressant medications.”",
+          },
+          {
+            data: "The NCCIH study “included more than 12,000 participants, and the researchers found that for treating anxiety and depression, mindfulness-based approaches were better than no treatment at all, and they worked as well as the evidence-based therapies. - Meditation And Mindfulness: What You Need To Know",
+          },
+        ],
+      },
+    ],
+
+    links: [
+      {
+        title: "Meditation and Mindfulness: What You Need To Know",
+        link: "https://www.nccih.nih.gov/health/meditation-and-mindfulness-what-you-need-to-know",
+      },
+      {
+        title: "How Meditation Can Improve Your Mental Health",
+        link: "https://www.everydayhealth.com/meditation/how-meditation-can-improve-your-mental-health/",
+      },
+    ],
+  },
+  { topic: "time-in-nature" },
+  { topic: "movement" },
+  { topic: "social-connection" },
+  { topic: "self-expression" },
+];
