20e3e1610a5d6bf433184a81c317677a9e2a7fa1
Jena Brown <brownjena755@gmail.com>
2023-10-18 19:00:03 -0400

    added signuppage

---
+++ client/src/ui/Signup.jsx
@@ -0,0 +1,10 @@
+import '@passageidentity/passage-elements/passage-register';
+
+function Signup() {
+  return (
+      <div>
+        <passage-register app-id="yKCRLJ0k5VFjLECim3ynXoSW"></passage-register>
+      </div>
+  );
+}
+export default Signup;
