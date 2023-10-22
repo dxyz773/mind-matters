a2a18a3f03154025098e0cf52326cc3d5d48cef5
Jena Brown <brownjena755@gmail.com>
2023-10-22 17:50:05 -0400

    centered Login component

--- client/src/ui/Login.jsx
+++ client/src/ui/Login.jsx
@@ -2,7 +2,7 @@ import '@passageidentity/passage-elements/passage-login';
 
 function Login() {
   return (
-      <div>
+     <div className=" p-4 mt-8 container mx-auto">
         <passage-login app-id="yKCRLJ0k5VFjLECim3ynXoSW"></passage-login>
       </div>
   );
