862e1660e48f1067a0f23faf1c54346eea786d8e
Jena Brown <brownjena755@gmail.com>
2023-10-22 17:59:08 -0400

    removed uneccessary callback functioned and centered element

--- client/src/ui/Signup.jsx
+++ client/src/ui/Signup.jsx
@@ -2,31 +2,14 @@ import '@passageidentity/passage-elements/passage-register';
 import React, { useEffect } from 'react';
 
 function Signup() {
-  
-  function checkvalidation(){
-    console.log("The identifier is "+ identifier );
-    return true
-  }
-  //add user to database here 
-  function beforeAuth(identifier) {
-   console.log("the identifier is "+ identifier)
-    return true
-  }
-
-  useEffect(() => {
-    const passageRegisterElement = document.querySelector('passage-register');
-
-    if (passageRegisterElement) {
-      // Set the beforeAuth callback
-      passageRegisterElement.beforeAuth = beforeAuth;
-    }
-  }, []);
-
   return (
-    <div>
-      <passage-register app-id="yKCRLJ0k5VFjLECim3ynXoSW"></passage-register>
-    </div>
-  
+<div class=" p-4 mt-8 container mx-auto">
+    <div className="text-center font-bold">
+    Register
+</div>
+        <passage-register app-id="yKCRLJ0k5VFjLECim3ynXoSW">
+        </passage-register>
+</div>
   );
 }
 
