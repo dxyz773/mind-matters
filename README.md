08ccd62c2ee8f896a7475a18b2be8a8a9614b5af
demitryedwards <dxyz773@gmail.com>
2023-10-22 20:26:29 -0400

    Syling

--- README.md
+++ README.md
@@ -1,23 +1,28 @@
 # Mind Matters
 
 ## Introduction
+
 This repository was created as part of the Women Who Code Hackathon For Social Code 2023. Mind Matters is an application designed to help individuals improve their well-being and mental health by providing a structured way to engage in self-care tasks. It offers a task tracking system to motivate users to perform these tasks regularly. In addition, the app provides a wealth of wellness resources and information on how to perform various self-care tasks effectively.
 
 ## Problem Statement
+
 Maintaining mental health and well-being is essential, but many people struggle to establish and stick to a self-care routine. Our app addresses this challenge by offering a solution that encourages users to engage in tasks that promote mental health and overall well-being. Moreover, we understand that individuals may need guidance on how to perform self-care tasks effectively.
 
 ## Key Features
+
 - **Task Tracking:** Users can create and track self-care tasks they want to perform regularly.
 - **Motivation:** The app provides a motivating system to encourage users to complete their tasks.
 - **User-Friendly Interface:** A simple and intuitive user interface for an enjoyable user experience.
 - **Wellness Resources:** Access resources explaining how to perform different activities to improve mental well-being.
 
 ## How It Works
+
 Users choose from a variety of self-care tasks organized by category. Users select the task they wish to perform. Users can monitor their task completion progress and track improvements in their well-being and mental health over time.
 
 ## Tech Stack
 
 ### Frontend
+
 The Well-Being and Mental Health Task Tracker utilizes a comprehensive tech stack to deliver a robust and engaging user experience.
 
 - **React**:
@@ -29,6 +34,7 @@ The Well-Being and Mental Health Task Tracker utilizes a comprehensive tech stac
 - **DaisyUI**
 
 ### Backend
+
 For the backend of our application, we have chosen a versatile tech stack that offers reliability and scalability:
 
 - **Node.js**
@@ -36,15 +42,18 @@ For the backend of our application, we have chosen a versatile tech stack that o
 - **Express.js**
 
 - **PostgreSQL**
+
 ### Design and Collaboration
+
 - **Figma**:
+
 ## Instructions to run client in development:
 
 #### FIRST TIME running client:
 
 1. Open new terminal in IDE/code editor
 2. cd into client folder
-2a. This is a test by Ray
+   2a. This is a test by Ray
 3. Install dependencies
 
 ```
@@ -77,14 +86,13 @@ npm run dev
 
 ---
 
-
 ### First Time adding .env variables:
+
 1. Make sure you have a .env file inside your backend folder
 2. go to the passage console for wellness warriors
 3. click on the mind matters application
 4. in the .env file paste the ap ID into PASSAGE_APP_ID=
 5. go to API Keys in the passage console
-6. create a new key 
+6. create a new key
 7. copy the value of that key into the .env file after PASSAGE_API_KEY=
-8. make sure you dont track the .env file 
-
+8. make sure you dont track the .env file
--- client/src/ui/Home.jsx
+++ client/src/ui/Home.jsx
@@ -1,28 +1,43 @@
 function Home() {
-  return <div>
-    <div id="homeIntro">
-      <h1 id="stay_on_track">Stay On Track With  Your Personal <span id="well-being">Well-being</span> Companion </h1>
-      <p id="receive_personalized">Receive personalized activity recommendations to boost your mental health and track your progress on a journey towards a happier, more balanced you. Let us be your guide to a better state of mind.</p>
-    </div>
-    <div id="homepageButtons">
-      <button id="signUpBtn">Sign Up</button>
-      <button id="learnMoreBtn">Learn More</button>
-    </div>
-    <div id="secondRowContainer">
-      <div id="ourMissionContainer">
-        <h2 id="missionHeading">Our Mission</h2>
-        <p id="missionStatement">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis felis velit, egestas vel dui sed, semper convallis quam. Cras id eros felis. Sed vehicula, ex vitae euismod suscipit, lacus erat sagittis dolor, non volutpat erat tellus ac velit. Phasellus placerat consequat risus in bibendum. Morbi condimentum dui sem, ut posuere purus lacinia id. Pellentesque in dolor vulputate, vehicula leo ac, ultricies nibh. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
-        <div id="learn-more-mission-btn-container"><button id="learnMoreMissionBtn">Learn More</button></div>
+  return (
+    <div>
+      <div id="homeIntro">
+        <h1 id="stay_on_track">
+          Stay On Track With Your Personal{" "}
+          <span id="well-being">Well-being</span> Companion{" "}
+        </h1>
+        <p id="receive_personalized">
+          Receive personalized activity recommendations to boost your mental
+          health and track your progress on a journey towards a happier, more
+          balanced you. Let us be your guide to a better state of mind.
+        </p>
       </div>
-      <div id="phoneTargetImage">IMAGE HERE!</div>
+      <div id="homepageButtons">
+        <button id="signUpBtn">Sign Up</button>
+        <button id="learnMoreBtn">Learn More</button>
+      </div>
+      <div id="secondRowContainer">
+        <div id="ourMissionContainer">
+          <h2 id="missionHeading">Our Mission</h2>
+          <p id="missionStatement">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis felis
+            velit, egestas vel dui sed, semper convallis quam. Cras id eros
+            felis. Sed vehicula, ex vitae euismod suscipit, lacus erat sagittis
+            dolor, non volutpat erat tellus ac velit. Phasellus placerat
+            consequat risus in bibendum. Morbi condimentum dui sem, ut posuere
+            purus lacinia id. Pellentesque in dolor vulputate, vehicula leo ac,
+            ultricies nibh. Orci varius natoque penatibus et magnis dis
+            parturient montes, nascetur ridiculus mus.{" "}
+          </p>
+          <div id="learn-more-mission-btn-container">
+            <button id="learnMoreMissionBtn">Learn More</button>
+          </div>
+        </div>
+        <div id="phoneTargetImage"></div>
+      </div>
     </div>
-
-  </div>
-
-
+  );
 }
 
 export default Home;
 Home;
-
-
